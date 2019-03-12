25527e91f4a44a2537071c242d7c362910384f0b
Telzhanov <askhat491@gmail.com>
2019-03-12 14:16:44 +0600

    merge problem

--- app/src/main/java/kz/validol/hacknu/auth/LoginActivity.kt
+++ app/src/main/java/kz/validol/hacknu/auth/LoginActivity.kt
@@ -1,29 +1,118 @@
 package kz.validol.hacknu.auth
 
+import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import android.util.Log
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.GraphRequest
+import com.facebook.login.LoginResult
+import kotlinx.android.synthetic.main.activity_login.*
 import kz.validol.hacknu.Api
 import kz.validol.hacknu.App
-import kz.validol.hacknu.R
-import kz.validol.hacknu.entities.User
 import org.koin.android.ext.android.inject
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     private val api: Api by inject()
     private val sharedPref: SharedPreferences by inject()
+    private val faceBookCallbackManager = CallbackManager.Factory.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(kz.validol.hacknu.R.layout.activity_login)
+        edit_text_sign_in_password.setTintColor(Color.parseColor("#c0c5ce"))
 
-        api.register(User(-1,"ezhan9800@gmail.com", "Yerzhan", "1234", 21, "Helllo2", App.fcmDeviceId))
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe()
+        loginViaFacebook()
+
+//        api.register(User(-1,"ezhan9800@gmail.com", "Yerzhan", "1234", 21, "Helllo2", App.fcmDeviceId))
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe()
+    }
+
+    private fun loginViaFacebook() {
+        facebookLogin.setOnClickListener {
+            login_button.performClick()
+        }
+        login_button.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"))
+        login_button.registerCallback(faceBookCallbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                App.facebookToken = loginResult.accessToken.token
+
+                val request = GraphRequest.newMeRequest(
+                    loginResult.accessToken
+                ) { obj, response ->
+                    obj.getString("id")?.let {
+                        Log.v("accepted", it)
+                    }
+                    obj.getString("birthday")?.let {
+                        Log.v("accepted", it)
+                    }
+                    obj.getString("first_name")?.let {
+                        Log.v("accepted", it)
+                    }
+                    if (obj.has("gender")) {
+                        obj.getString("gender")?.let {
+                            Log.v("accepted", it)
+                        }
+                    }
+                    obj.getString("last_name")?.let {
+                        Log.v("accepted", it)
+                    }
+                    if (obj.has("link")) {
+                        obj.getString("link")?.let {
+                            Log.v("accepted", it)
+                        }
+                    }
+                    if (obj.has("location")) {
+                        obj.getString("location")?.let {
+                            Log.v("accepted", it)
+                        }
+                    }
+                    if (obj.has("locale")) {
+                        obj.getString("locale")?.let {
+                            Log.v("accepted", it)
+                        }
+                    }
+                    obj.getString("name")?.let {
+                        Log.v("accepted", it)
+                    }
+                    if (obj.has("email")) {
+                        obj.getString("email")?.let {
+                            Log.v("accepted", it)
+                        }
+                    }
+                }
+                val parameters = Bundle()
+                parameters.putString(
+                    "fields",
+                    "id,name,email,gender,birthday,first_name,last_name,link,location,locale"
+                )
+                request.parameters = parameters
+                request.executeAsync()
+            }
+
+            override fun onCancel() {
+                // App code
+            }
+
+            override fun onError(exception: FacebookException) {
+                // App code
+            }
+        })
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        faceBookCallbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 }
--- app/src/main/java/kz/validol/hacknu/auth/RegisterActivity.kt
+++ app/src/main/java/kz/validol/hacknu/auth/RegisterActivity.kt
@@ -1,12 +1,18 @@
 package kz.validol.hacknu.auth
 
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_register.*
+import kz.validol.hacknu.Api
 import kz.validol.hacknu.R
+import org.koin.android.ext.android.inject
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val api: Api by inject()
+    private val sharedPref: SharedPreferences by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
