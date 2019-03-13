7d634c1e70b34f565f9b9f56b7bd0f3952b9ec94
Nur <nur.almakhanov@gmail.com>
2019-03-14 00:14:36 +0600

    gitHUB

--- app/src/main/java/kz/validol/hacknu/auth/LoginActivity.kt
+++ app/src/main/java/kz/validol/hacknu/auth/LoginActivity.kt
@@ -3,33 +3,41 @@ package kz.validol.hacknu.auth
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.GraphRequest
 import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GithubAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 import kz.validol.hacknu.Api
 import kz.validol.hacknu.App
+import okhttp3.*
 import org.koin.android.ext.android.inject
+import java.io.IOException
+import java.math.BigInteger
+import java.security.SecureRandom
 import java.util.*
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import android.R.attr.data
-import com.google.android.gms.common.api.ApiException
-//import android.support.test.orchestrator.junit.BundleJUnitUtils.getResult
-import android.support.v4.app.FragmentActivity
-import com.google.android.gms.tasks.Task
 
 
 class LoginActivity : AppCompatActivity() {
 
     private val api: Api by inject()
+    private val random = SecureRandom()
+    private var mAuth: FirebaseAuth? = null
+    private var mAuthListener: FirebaseAuth.AuthStateListener? = null
     private val sharedPref: SharedPreferences by inject()
     private val faceBookCallbackManager = CallbackManager.Factory.create()
     private var mGoogleSignInClient:GoogleSignInClient? = null
@@ -41,6 +49,7 @@ class LoginActivity : AppCompatActivity() {
             .requestEmail()
             .build()
         loginViaFacebook()
+        loginViaGithub()
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
 //        api.register(User(-1,"ezhan9800@gmail.com", "Yerzhan", "1234", 21, "Helllo2", App.fcmDeviceId))
@@ -166,6 +175,111 @@ class LoginActivity : AppCompatActivity() {
 //            updateUI(null)
         }
 
+    }
+
+    private fun loginViaGithub() {
+        mAuth = FirebaseAuth.getInstance()
+
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            val user = it.currentUser
+            if(user != null){
+                RegisterActivity.signedGIthub = true
+                Log.d("accepted", user.email)
+            }
+        }
+
+        //Called after the github server redirect us to REDIRECT_URL_CALLBACK
+        val uri = intent.data
+        if (uri != null && uri.toString().startsWith(RegisterActivity.REDIRECT_URL_CALLBACK)) {
+            val code = uri.getQueryParameter("code")
+            val state = uri.getQueryParameter("state")
+            if (code != null && state != null)
+                sendPost(code, state)
+        }
+
+        github.setOnClickListener{
+            signInOut()
+        }
+    }
+    fun signInOut() {
+        if (!RegisterActivity.signedGIthub) {
+            //https://developer.github.com/apps/building-integrations/setting-up-and-registering-oauth-apps/about-authorization-options-for-oauth-apps/
+            //GET http://github.com/login/oauth/authorize
+            val httpUrl = HttpUrl.Builder()
+                .scheme("http")
+                .host("github.com")
+                .addPathSegment("login")
+                .addPathSegment("oauth")
+                .addPathSegment("authorize")
+                .addQueryParameter("client_id", "2d22cb3f15cda9027eb1")
+                .addQueryParameter("redirect_uri", RegisterActivity.REDIRECT_URL_CALLBACK)
+                .addQueryParameter("state", getRandomString())
+                .addQueryParameter("scope", "user:email")
+                .build()
+
+            Log.d("accepted", httpUrl.toString())
+
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(httpUrl.toString()))
+            startActivity(intent)
+
+        } else {
+            mAuth?.signOut()
+        }
+    }
+
+    private fun getRandomString(): String {
+        return BigInteger(130, random).toString(32)
+    }
+
+    private fun sendPost(code: String, state: String) {
+        //POST https://github.com/login/oauth/access_token
+
+        val okHttpClient = OkHttpClient()
+        val form = FormBody.Builder()
+            .add("client_id", "2d22cb3f15cda9027eb1")
+            .add("client_secret", "aca85697d9f0d5b1b1342e349f8786548df5689c")
+            .add("code", code)
+            .add("redirect_uri", RegisterActivity.REDIRECT_URL_CALLBACK)
+            .add("state", state)
+            .build()
+
+        val request = Request.Builder()
+            .url("https://github.com/login/oauth/access_token")
+            .post(form)
+            .build()
+
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                Toast.makeText(this@LoginActivity, "onFailure: " + e.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                //access_token=e72e16c7e42f292c6912e7710c838347ae178b4a&token_type=bearer
+                val responseBody = response.body()?.string()
+                val splitted = responseBody?.split("=|&".toRegex())?.dropLastWhile({ it.isEmpty() })!!.toTypedArray()
+                if (splitted[0].equals("access_token", ignoreCase = true))
+                    signInWithToken(splitted[1])
+                else
+                    Toast.makeText(this@LoginActivity, "splitted[0] =>" + splitted[0], Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    fun signInWithToken(token: String) {
+        val credential = GithubAuthProvider.getCredential(token)
+        mAuth?.signInWithCredential(credential)
+            ?.addOnCompleteListener(this) {
+                Log.d("accepted", "signInWithCredential:onComplete:" + it.isSuccessful)
+
+                if (!it.isSuccessful) {
+                    it.exception?.printStackTrace()
+                    Log.w("accepted", "signInWithCredential", it.exception)
+                    Toast.makeText(this@LoginActivity, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
 }
--- app/src/main/java/kz/validol/hacknu/auth/RegisterActivity.kt
+++ app/src/main/java/kz/validol/hacknu/auth/RegisterActivity.kt
@@ -3,26 +3,47 @@ package kz.validol.hacknu.auth
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.GraphRequest
 import com.facebook.login.LoginResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GithubAuthProvider
 import kotlinx.android.synthetic.main.activity_register.*
 import kz.validol.hacknu.Api
 import kz.validol.hacknu.App
+import okhttp3.*
 import org.koin.android.ext.android.inject
+import java.io.IOException
+import java.math.BigInteger
+import java.security.SecureRandom
 import java.util.*
 
 
 class RegisterActivity : AppCompatActivity() {
 
+    companion object {
+        val REDIRECT_URL_CALLBACK = "epam://git.oauth2token"
+        var signedGIthub = false
+    }
+    private val random = SecureRandom()
     private val api: Api by inject()
     private val sharedPref: SharedPreferences by inject()
     private val faceBookCallbackManager = CallbackManager.Factory.create()
+    private var mAuth: FirebaseAuth? = null
+    private var mAuthListener: FirebaseAuth.AuthStateListener? = null
+
+
+    override fun onStart() {
+        super.onStart()
+        mAuth?.addAuthStateListener(mAuthListener!!)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +53,7 @@ class RegisterActivity : AppCompatActivity() {
 
         loginViaFacebook()
 
-        loginViaTwitter()
+        loginViaGithub()
 
 //        api.register(User(-1,"ezhan9800@gmail.com", "Yerzhan", "1234", 21, "Helllo2", App.fcmDeviceId))
 //            .subscribeOn(Schedulers.io())
@@ -40,11 +61,29 @@ class RegisterActivity : AppCompatActivity() {
 //            .subscribe()
     }
 
-    private fun loginViaTwitter() {
-        twitter.setOnClickListener{
+    private fun loginViaGithub() {
+        mAuth = FirebaseAuth.getInstance()
 
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            val user = it.currentUser
+            if(user != null){
+                signedGIthub = true
+                Log.d("accepted", user.email)
+            }
         }
 
+        //Called after the github server redirect us to REDIRECT_URL_CALLBACK
+        val uri = intent.data
+        if (uri != null && uri.toString().startsWith(REDIRECT_URL_CALLBACK)) {
+            val code = uri.getQueryParameter("code")
+            val state = uri.getQueryParameter("state")
+            if (code != null && state != null)
+                sendPost(code, state)
+        }
+
+        github.setOnClickListener{
+            signInOut()
+        }
     }
 
     private fun loginViaFacebook() {
@@ -123,5 +162,86 @@ class RegisterActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         faceBookCallbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun signInOut() {
+        if (!signedGIthub) {
+            //https://developer.github.com/apps/building-integrations/setting-up-and-registering-oauth-apps/about-authorization-options-for-oauth-apps/
+            //GET http://github.com/login/oauth/authorize
+            val httpUrl = HttpUrl.Builder()
+                .scheme("http")
+                .host("github.com")
+                .addPathSegment("login")
+                .addPathSegment("oauth")
+                .addPathSegment("authorize")
+                .addQueryParameter("client_id", "2d22cb3f15cda9027eb1")
+                .addQueryParameter("redirect_uri", REDIRECT_URL_CALLBACK)
+                .addQueryParameter("state", getRandomString())
+                .addQueryParameter("scope", "user:email")
+                .build()
+
+            Log.d("accepted", httpUrl.toString())
+
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(httpUrl.toString()))
+            startActivity(intent)
+
+        } else {
+            mAuth?.signOut()
+        }
+    }
+
+    private fun getRandomString(): String {
+        return BigInteger(130, random).toString(32)
+    }
+
+    private fun sendPost(code: String, state: String) {
+        //POST https://github.com/login/oauth/access_token
+
+        val okHttpClient = OkHttpClient()
+        val form = FormBody.Builder()
+            .add("client_id", "2d22cb3f15cda9027eb1")
+            .add("client_secret", "aca85697d9f0d5b1b1342e349f8786548df5689c")
+            .add("code", code)
+            .add("redirect_uri", REDIRECT_URL_CALLBACK)
+            .add("state", state)
+            .build()
+
+        val request = Request.Builder()
+            .url("https://github.com/login/oauth/access_token")
+            .post(form)
+            .build()
+
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                Toast.makeText(this@RegisterActivity, "onFailure: " + e.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                //access_token=e72e16c7e42f292c6912e7710c838347ae178b4a&token_type=bearer
+                val responseBody = response.body()?.string()
+                val splitted = responseBody?.split("=|&".toRegex())?.dropLastWhile({ it.isEmpty() })!!.toTypedArray()
+                if (splitted[0].equals("access_token", ignoreCase = true))
+                    signInWithToken(splitted[1])
+                else
+                    Toast.makeText(this@RegisterActivity, "splitted[0] =>" + splitted[0], Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    fun signInWithToken(token: String) {
+        val credential = GithubAuthProvider.getCredential(token)
+        mAuth?.signInWithCredential(credential)
+            ?.addOnCompleteListener(this) {
+                Log.d("accepted", "signInWithCredential:onComplete:" + it.isSuccessful)
+
+                if (!it.isSuccessful) {
+                    it.exception?.printStackTrace()
+                    Log.w("accepted", "signInWithCredential", it.exception)
+                    Toast.makeText(this@RegisterActivity, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
