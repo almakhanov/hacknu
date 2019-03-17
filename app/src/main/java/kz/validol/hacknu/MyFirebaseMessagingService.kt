87ea627e3b20428b014a9b81a7896fc84426622e
Nur <nur.almakhanov@gmail.com>
2019-03-17 13:55:42 +0600

    ff

--- app/src/main/java/kz/validol/hacknu/MyFirebaseMessagingService.kt
+++ app/src/main/java/kz/validol/hacknu/MyFirebaseMessagingService.kt
@@ -11,7 +11,6 @@ import android.support.v4.app.NotificationCompat
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import kz.validol.hacknu.auth.LoginActivity
 
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
@@ -41,7 +40,7 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
     }
 
     private fun sendNotification(messageBody: String) {
-        val intent = Intent(this, LoginActivity::class.java)
+        val intent = Intent(this, SplashActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT)
